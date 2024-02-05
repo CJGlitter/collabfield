0108d43c118c6e7709659f1bb7dc9684f9a98469
Cory Davis <YellowDragon2009@gmail.com>
2024-02-05 11:06:39 -0600

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into   partials and extract logic from the file into partials

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,2 +1,11 @@
 module NavigationHelper
+
+  def collapsible_links_partial_path
+    if user_singed_in?
+      'layouts/navigation/collapsible_elements/signed_in_links'
+    else
+      'layouts/navigation/collapsible_elements/non_singed_in_links'
+    end
+  end
+
 end
